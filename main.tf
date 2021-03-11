ec8d300c083bdfb3e19c61a153f01a87fb2a6a97
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-11 21:49:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "panos_panorama_device_group" "this" {
     name = "policy-as-code"
-    description = "example device group for policy as code demo"
+    description = "example device group for policy as code demo."
 }
