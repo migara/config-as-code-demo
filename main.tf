a15cb64658e1de1d72f9e8bc92b8b15610a5e084
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2024-05-02 01:47:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "panos_panorama_security_rule_group" "this" {
       source_zones = lookup(rule.value, "source_zones", ["any"])
       source_addresses = lookup(rule.value, "source_addresses", ["any"])
       source_users = lookup(rule.value, "source_users", ["any"])
-      hip_profiles = lookup(rule.value, "hip_profiles", ["any"])
       destination_zones = lookup(rule.value, "destination_zones", ["any"])
       destination_addresses = lookup(rule.value, "destination_addresses", ["any"])
       applications = lookup(rule.value, "applications", ["any"])
